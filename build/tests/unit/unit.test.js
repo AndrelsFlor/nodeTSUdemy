b5fdc799bb9adb293a69ba4cb109c896b035d723
andre <andre@localhost.localdomain>
2018-06-13 23:00:46 -0400

    inicial - testes de unidade escritos

---
+++ build/tests/unit/unit.test.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+describe('Testes Unitários do Controller', () => {
+    describe('Método Create', () => {
+        it('Deve criar um novo Usuário', () => {
+        });
+    });
+    describe('Método update', () => {
+        it('Deve atualizar um usuário', () => {
+        });
+    });
+    describe('Método GET User', () => {
+        it('Deve retornar uma lista com todos os usuários', () => {
+        });
+    });
+    describe('Método DELETE User', () => {
+        it('Deve deletar um usuário', () => {
+        });
+    });
+});
---
+++ tests/unit/unit.test.ts
@@ -0,0 +1,27 @@
+import {testDouble, expect} from './config/helpers';
+
+describe('Testes Unitários do Controller', () => {
+    describe('Método Create', () => {
+        it('Deve criar um novo Usuário', () => {
+
+        });
+    });
+
+    describe('Método update', () => {
+        it('Deve atualizar um usuário', () => {
+
+        });
+    });
+
+    describe('Método GET User', () => {
+        it('Deve retornar uma lista com todos os usuários', () => {
+
+        });
+    });
+
+    describe('Método DELETE User', () => {
+        it('Deve deletar um usuário', () => {
+
+        });
+    });
+});
